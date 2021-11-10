e284a38a92bb5b16e66180fa9fbeb847870cc08b
c4benni <62204922+c4benni@users.noreply.github.com>
2021-11-10 15:59:14 +0100

    update

---
+++ _nuxt/static/1636556232/recipe/ykr-kuykq7wm/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/recipe/ykr-kuykq7wm", (function(a){return {data:[{}],fetch:{},mutations:[[a,{name:"greeting",value:"afternoon"}],[a,{path:"text",value:"Recipe",innerPath:"header"}]]}}("UPDATE_")));
